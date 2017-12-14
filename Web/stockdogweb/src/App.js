b41bb5bd82e872f33df47e283a21ec604ce5d4fe
Ashley Newman <ashznewman@gmail.com>
2017-12-14 00:08:32 +0000

    initial web commit

---
+++ Web/stockdogweb/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <button type="button" class="btn btn-primary">API Call</button>
+      </div>
+    );
+  }
+}
+
+export default App;
