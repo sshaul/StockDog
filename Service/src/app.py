1769c4987539a4a3aac906b1ac605837729ad24a
sthanawa <sthanawa@calpoly.edu>
2018-02-06 00:39:23 -0800

    connecting to db - password not shown

--- Service/src/app.py
+++ Service/src/app.py
@@ -2,20 +2,22 @@ import argparse
 from util import logger
 
 from flask import Flask
-from flask import make_response
+from flask import make_response, request
 from flask import jsonify
 from flask_cors import CORS
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask import jsonify
+import pymysql
+from stock import stock_api
+#from login import login_api
 
 
 app = Flask(__name__)
 
-from stock import stock_api
-from login import login_api
-
 CORS(app)
 
 app.register_blueprint(stock_api)
-app.register_blueprint(login_api)
+#app.register_blueprint(login_api)
 
 log = logger.Logger(True, True, True)
 
@@ -37,5 +39,41 @@ def getPortNum(defaultPort=5005):
    return args.port or defaultPort 
 
 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#connecting to db
+#is host and port and db required
+conn = pymysql.connect(user= "root", password = "", database="Stockdog")
+#used to execute queries
+c = conn.cursor()
+#c.execute("show tables")
+
+
+#used for debugging - making sure that the tables are accessible
+#for (table_name,) in c:
+#   log.info(table_name)
+#@app.route('/user/login', methods = ['POST'])
+
+
+
+
+#class User(UserMixin):
+#    def __init__(self, id):
+#        self.id = id
+#        self.name = "user" + str(id)
+#        self.password = self.name + "_secret"
+
+#    def __repr__(self):
+#        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+
+#@login_manager.user_loader
+#def load_user(user_id):
+#    return User.get(user_id)
+
 if __name__ == '__main__':
    app.run(debug=True, port=getPortNum(), host='0.0.0.0')
+
