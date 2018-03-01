adbe2ff5700fee543959cf70695f5b892ff455ee
Nishanth <nishanth.dara@gmail.com>
2018-02-28 21:12:45 -0800

    Refactored nuke

--- Service/src/nuke.py
+++ Service/src/nuke.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response
+from flask import Blueprint, request, Response
 from util import logger
 import pymysql
 import dbConn
@@ -9,19 +9,21 @@ nuke_api = Blueprint('nuke_api', __name__)
 
 @nuke_api.route('/api/nuke', methods=['POST'])
 def post_nuke():
+   body = request.get_json()
    try:
       conn = dbConn.getDBConn()
       cursor = conn.cursor()
    except Exception as e:
       return Response('Failed to make connection to database', status=500)
 
-   cursor.execute("DELETE FROM PortfolioItem")
-   cursor.execute("DELETE FROM PortfolioHistory")
-   cursor.execute("DELETE FROM Watchlist")
-   cursor.execute("DELETE FROM Transaction")
-   cursor.execute("DELETE FROM Ticker")
-   cursor.execute("DELETE FROM Portfolio")
-   cursor.execute("DELETE FROM User")
+   delete_tables(cursor, ['PortfolioItem', 'PortfolioHistory', 'Watchlist', 'Transaction', 
+      'Ticker', 'Portfolio', 'User'], body['resetIncrement'])
 
    conn.commit()
    return Response(status=200)
+
+def delete_tables(cursor, tables, resetIncrement):
+   for table in tables:
+      cursor.execute("DELETE FROM " + table)
+      if resetIncrement:
+         cursor.execute("ALTER TABLE " + table + " AUTO_INCREMENT=1")
