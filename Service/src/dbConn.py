9c6cb78efc1d8e2cde7af8c764c095c6b63c853d
Ashley Newman <ashznewman@gmail.com>
2018-03-01 00:05:49 -0800

    Updated correct db name

--- Service/src/dbConn.py
+++ Service/src/dbConn.py
@@ -9,7 +9,7 @@ def getDBConn():
    except Exception as e:
       raise Exception('The filename was not provided or poorly formatted') 
 
-   conn = pymysql.connect(user=config['username'], password=config['password'], database='StockDog',
+   conn = pymysql.connect(user=config['username'], password=config['password'], database='Stockdog',
       cursorclass=pymysql.cursors.DictCursor)
 
    return conn
