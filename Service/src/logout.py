2e39097836d4a72fe5b848bcca93251180bfc1f5
Nishanth <nishanth.dara@gmail.com>
2018-04-23 15:46:19 -0700

    Removed unnecessary code and used g object

--- Service/src/logout.py
+++ Service/src/logout.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, g
 import dbConn
 from util import logger
 
@@ -9,21 +9,7 @@ log = logger.Logger(True, True, True)
 @logout_api.route('/api/logout', methods=['DELETE'])
 def logout_user():
     body = request.get_json()
-    try:
-        conn = dbConn.getDBConn()
-        cursor = conn.cursor()
-    except Exception as e:
-        return Response('Failed to make connection to database', status=500)
 
-    try:
-        cursor.execute('UPDATE USER SET token = NULL where id = %s', body['userId'])
-    except Exception as e:
-       return Response('Failed to execute query', status=500)
+    g.cursor.execute('UPDATE USER SET token = NULL where id = %s', body['userId'])
 
-    conn.commit()
-    cursor.execute('SELECT token from USER WHERE id = %s', body['userId'])
-    userInfo = cursor.fetchone()
-    if (userInfo['token'] == None):
-        return Response('Removed token and user has been logged out', status=200)
-    else:
-        return Response('Failed to remove token', status=500)
+    return Response(status=200)
