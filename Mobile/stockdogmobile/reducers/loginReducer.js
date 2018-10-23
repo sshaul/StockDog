8f5d9167eb82adfc01abc978a97fb76cb5d00956
sshaul <sshaul@calpoly.edu>
2018-10-22 19:04:27 -0700

    Update loginReducer.js

--- Mobile/stockdogmobile/reducers/loginReducer.js
+++ Mobile/stockdogmobile/reducers/loginReducer.js
@@ -1,7 +1,6 @@
 import { ACTION_TYPES } from '../actions/actionTypes';
 
 export default loginReducer = (state = {user: null, password: null}, action) => {
-    console.log('in login reducer: ', action.type);
     switch(action.type) {
         case ACTION_TYPES.LOGIN_USER:
             console.log('logging in: ', action);
