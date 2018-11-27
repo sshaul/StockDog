e598249043a1318ed4aebd436e2ee3129c601cf8
asnewman <ashznewman@gmail.com>
2018-11-26 21:59:44 -0800

    Transfer

---
+++ Web/stockdogweb/src/containers/Portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navigation/Navbar';
+
+class Portfolio extends Component {
+   render() {
+      return (
+         <div className="Portfolio">
+            <Navbar />
+         </div>
+      );
+   }
+}
+
+export default Portfolio;
