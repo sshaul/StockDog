0ea87d4588bebd5538970c8edd40289ac98230d8
asnewman <ashznewman@gmail.com>
2018-11-28 06:52:55 -0800

    Added Graph

--- Web/stockdogweb/src/containers/Portfolio/Portfolio.js
+++ Web/stockdogweb/src/containers/Portfolio/Portfolio.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/Navigation/Navbar';
+import Graph from '../../components/Graph/Graph';
+import Listing from '../../components/Listing/Listing';
+
+import './Portfolio.css';
 
 class Portfolio extends Component {
    render() {
       return (
          <div className="Portfolio">
-            <Navbar />
-         </div>
+            <Navbar links={[
+               {
+                  title: "Month League",
+                  location: "/league/monthLeague"
+               },
+               {
+                  title: "Penny Stocks",
+                  location: "/league/pennyStocks"
+               },
+               {
+                  title: "Swing Stocks",
+                  location: "/league/swingStocks"
+               }
+            ]
+            }/>
+         <Graph />
+         {/*<Listing />
+         <Listing />*/}
+      </div>
       );
    }
 }
