6478846d4cc857aed6d893de8e99b4133146ad4a
asnewman <ashznewman@gmail.com>
2018-11-28 23:02:00 -0800

    Fixed issue of not rendering listings

--- Web/stockdogweb/src/components/Listing/Listing.js
+++ Web/stockdogweb/src/components/Listing/Listing.js
@@ -15,7 +15,7 @@ class Listing extends Component {
       var listingElements = [];
 
       // Dont' do anything if there are no listings
-      if (!this.props.listing) {
+      if (!this.props.listings) {
          return;
       }
 
@@ -26,43 +26,47 @@ class Listing extends Component {
          var priceChangeElement;
 
          if (listing.priceChange >= 0) {
-            priceChangeElement = <div class="listing-item-price-change listing-up-color">
-               (+{listing.priceChange})
-            </div>
+            priceChangeElement = 
+               <div className="listing-item-price-change listing-up-color">
+                  (+{listing.priceChange})
+               </div>
          }
          else {
-            priceChangeElement = <div class="listing-item-price-change listing-down-color">
-               ({listing.priceChange})
-            </div>
+            priceChangeElement = 
+               <div className="listing-item-price-change listing-down-color">
+                  ({listing.priceChange})
+               </div>
          }
 
          // Need to do amount separately since 0 shows nothing
          var amountElement = <div></div>;
          if (listing.amount !== 0) {
-            amountElement = <div class="listing-item-amount">{listing.amount} shares</div>
+            amountElement = 
+               <div className="listing-item-amount">
+                  {listing.amount} shares
+               </div>
          }
 
          listingElements.push(
-            <div class="listing-item">
-               <div class="listing-item-title">{listing.title}</div>
-               <div class="listing-item-desc">{listing.desc}</div>
-               <div class="listing-item-price-info">
-                  <div class="listing-item-price">${listing.price}</div>
+            <div className="listing-item">
+               <div className="listing-item-title">{listing.title}</div>
+               <div className="listing-item-desc">{listing.desc}</div>
+               <div className="listing-item-price-info">
+                  <div className="listing-item-price">${listing.price}</div>
                   {priceChangeElement}
                </div>
                {amountElement}
             </div>
          )
       });
-
       this.setState({ listingElements });
    }
 
    render() {
       return (
-         <div class="Listing">
-            <div class="listing-title">{this.props.title}</div>
-            <div class="listing-items">
+         <div className="Listing">
+            <div className="listing-title">{this.props.title}</div>
+            <div className="listing-items">
                {this.state.listingElements}
             </div>
          </div>
