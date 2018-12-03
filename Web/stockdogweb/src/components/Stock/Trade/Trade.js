8c46ff6197cd3d44523b1c9324acf5bcb9ddd10c
asnewman <ashznewman@gmail.com>
2018-12-02 18:24:24 -0800

    Trade information

--- Web/stockdogweb/src/components/Stock/Trade/Trade.js
+++ Web/stockdogweb/src/components/Stock/Trade/Trade.js
@@ -5,6 +5,20 @@ class Trade extends Component {
    render() {
       return (
          <div className='Trade'>
+            <div className='trade-elements'>
+               <div className='trade-element'>
+                  <h1>13</h1>
+                  <h2>Owned</h2>
+               </div>
+               <div className='trade-element'>
+                  <h1>$20.15</h1>
+                  <h2>Price</h2>
+               </div>
+               <div className='trade-element'>
+                  <h1>12M</h1>
+                  <h2>Volume</h2>
+               </div>
+            </div>
          </div>
       );
    }
