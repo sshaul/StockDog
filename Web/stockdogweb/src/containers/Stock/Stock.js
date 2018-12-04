07347e64f4c5193339b42a8959ccdd10c4f45492
asnewman <ashznewman@gmail.com>
2018-12-03 22:56:10 -0800

    Moved things to state

--- Web/stockdogweb/src/containers/Stock/Stock.js
+++ Web/stockdogweb/src/containers/Stock/Stock.js
@@ -7,13 +7,20 @@ import Article from '../../components/Article/Article';
 import Trade from '../../components/Trade/Trade';
 
 class Stock extends Component {
+   constructor(props) {
+      this.state = {
+         title: "About",
+         content: "Advanced Micro Devices, Inc. engages in the provision of semiconductor businesses. It operates through the Computing and Graphics and Enterprise, Embedded and Semi-Custom segments."
+      };
+   }
+
    render() {
       return (
          <div className="Stock">
             <Navbar />
             <Graph />
             <div className="stock-content">
-               <Article title={"About"} content={'Advanced Micro Devices, Inc. engages in the provision of semiconductor businesses. It operates through the Computing and Graphics and Enterprise, Embedded and Semi-Custom segments.'}/>
+               <Article title={this.state.title} content={this.state.content}/>
             </div>
             <Trade quantity={13} price={20.15} volume={'12M'}  />
          </div>
