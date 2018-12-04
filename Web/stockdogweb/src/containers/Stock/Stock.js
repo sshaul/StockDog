debdb95b42e49c27195b058dcac854c45086e1e9
asnewman <ashznewman@gmail.com>
2018-12-03 22:56:53 -0800

    Super call

--- Web/stockdogweb/src/containers/Stock/Stock.js
+++ Web/stockdogweb/src/containers/Stock/Stock.js
@@ -8,6 +8,7 @@ import Trade from '../../components/Trade/Trade';
 
 class Stock extends Component {
    constructor(props) {
+      super(props);
       this.state = {
          title: "About",
          content: "Advanced Micro Devices, Inc. engages in the provision of semiconductor businesses. It operates through the Computing and Graphics and Enterprise, Embedded and Semi-Custom segments."
