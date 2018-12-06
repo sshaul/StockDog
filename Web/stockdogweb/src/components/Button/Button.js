deabea490aa7a8e1a9b976c1d66ce3bd82327f93
asnewman <ashznewman@gmail.com>
2018-12-06 00:16:27 -0800

    Added ability to disable button

--- Web/stockdogweb/src/components/Button/Button.js
+++ Web/stockdogweb/src/components/Button/Button.js
@@ -5,7 +5,8 @@ class Button extends Component {
    render() {
       return (
          <div className="Button">
-            <button type="button" style={{width: this.props.width}}>
+            <button type="button" style={{width: this.props.width}}
+            disabled={this.props.isDisabled}>
                {this.props.text}
             </button>
          </div>
